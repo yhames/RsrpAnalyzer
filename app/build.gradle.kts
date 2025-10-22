008b614a72e5c9cd53d1f168df64397bc985ad0b
yhames <wjddn494@naver.com>
2025-10-22 15:41:33 +0900

    feat: Add Kakao Maps SDK
    Adds the Kakao Maps SDK dependency to the project.
    
    This includes:
    - Adding the Kakao Maps repository to `settings.gradle.kts`.
    - Defining the `kakao-maps-android` library in `libs.versions.toml`.
    - Implementing the library in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-}
+    implementation(libs.kakao.maps.android)}
